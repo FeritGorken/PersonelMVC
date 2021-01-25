92d660d83ea59715ec45f6ff9a128bb228e0a6a4
Ferit GÖRKEN <feritgorken1@gmail.com>
2021-01-25 17:49:28 +0300

    Ajax işlemleri , bootbox popup işlemleri

--- PersonelMVC/Scripts/custom.js
+++ PersonelMVC/Scripts/custom.js
@@ -1,4 +1,22 @@
-﻿function CheckDateTimeChange(dateElement) {
+﻿$(function () {
+    $("#tblDepartmanlar").on("click", ".btnDepartmanSil", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinizden emin misiniz?", function (result) {
+            if (result) {
+                var id = btn.data("id");
+                $.ajax({
+                    type: "GET",
+                    url: "/Departman/Sil/" + id,
+                    success: function () {
+                        btn.closest("tr").remove();
+                    }
+                });
+            }
+        });
+    });
+});
+
+function CheckDateTimeChange(dateElement) {
     var value = $(dateElement).val();
     if (value == '') {
         $(dateElement).valid("false");
